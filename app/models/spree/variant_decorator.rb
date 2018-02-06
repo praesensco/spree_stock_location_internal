9e699f15f0997a61e646b49535b6217cf4567371
Paweł Strzałkowski <pawel@praesens.co>
2018-02-06 22:10:30 +0100

    Add in_stock_external? to variant

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,18 +1,34 @@
 module Spree
   Variant.class_eval do
-    def total_on_hand_external
-      quantifier_external.total_on_hand
+    after_touch :clear_in_stock_external_cache
+
+    def backorderable_external?
+      quantifier_external.backorderable?
     end
 
     def can_supply_external?
       quantifier_external.can_supply?
     end
 
-    def backorderable_external?
-      quantifier_external.backorderable?
+    def in_stock_external?
+      Rails.cache.fetch(in_stock_external_cache_key) do
+        total_on_hand_external > 0
+      end
+    end
+
+    def total_on_hand_external
+      quantifier_external.total_on_hand
     end
 
     private
+
+    def clear_in_stock_external_cache
+      Rails.cache.delete(in_stock_external_cache_key)
+    end
+
+    def in_stock_external_cache_key
+      "variant-#{id}-in_stock_external"
+    end
 
     def quantifier_external
       Spree::Stock::Quantifier.new(self, false)
